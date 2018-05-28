714e49f52fd300221df69bbf31c93ab02c5a190e
Shashi Bangera <shashi@Shashis-MacBook-Pro.local>
2018-05-28 17:41:08 +0100

    my first commit

--- handler.js
+++ handler.js
@@ -2,7 +2,6 @@
 
 module.exports.hello = (event, context, callback) => {
   const response = {
-    animal: 'lion',
     APP_VAR: process.env.APP_VAR,
     SECRET_APP_VAR: process.env.SECRET_APP_VAR,
   };
